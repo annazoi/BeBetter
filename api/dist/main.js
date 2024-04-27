1a675489dbc466bfcbc6360886f981683889a628
Anna Zoi <zoianna01@gmail.com>
2024-04-27 15:59:35 +0300

    asdd

--- api/dist/main.js
+++ api/dist/main.js
@@ -1,10 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    app.setGlobalPrefix("api/v1");
+    app.enableCors({ origin: "*" });
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+    }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle("e-Closet API")
+        .setDescription("The e-Closet API description")
+        .setVersion("1.0")
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup("api/docs", app, document);
+    await app.listen(3333);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,21 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix("api/v1");
+  app.enableCors({ origin: "*" });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  );
+  const config = new DocumentBuilder()
+    .setTitle("e-Closet API")
+    .setDescription("The e-Closet API description")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document);
+  await app.listen(3333);
 }
 bootstrap();
