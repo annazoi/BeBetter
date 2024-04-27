578a55fcbd8be011527bbbd0c5f6a7b30e2faf2b
annazoi <zoianna01@gmail.com>
2024-04-27 15:38:43 +0300

    asd

---
+++ api/src/api/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Sidebar,
+  Visibility,
+  SegmentInline,
+  SegmentGroup,
+} from "semantic-ui-react";
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Button>hii</Button>
+    </div>
+  );
+};
+
+export default Home;
